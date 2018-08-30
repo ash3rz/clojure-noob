7ba7692bfdd74875e553010825bfa8979fbfc6ce
Ash <asherz@email.arizona.edu>
2018-08-29 17:42:47 -0700

    Finish hitting hobbits

--- src/clojure_noob/core.clj
+++ src/clojure_noob/core.clj
@@ -45,3 +45,23 @@
                      (set [part (matching-part part)])))))))
 
 (symmetrize-body-parts asym-hobbit-body-parts)
+
+(defn better-symmetrize-body-parts
+  "Expects a seq of maps that have a :name and :size"
+  [asym-body-parts]
+  (reduce (fn [final-body-parts part]
+            (into final-body-parts (set [part (matching-part part)])))
+          []
+          asym-body-parts))
+
+
+(defn hit
+  [asym-body-parts]
+  (let [sym-parts (better-symmetrize-body-parts asym-body-parts)
+        body-part-size-sum (reduce + (map :size sym-parts))
+        target (rand body-part-size-sum)]
+    (loop [[part & remaining] sym-parts
+            accumulated-size (:size part)]
+       (if (> accumulated-size target)
+         part
+         (recur remaining (+ accumulated-size (:size (first remaining))))))))
